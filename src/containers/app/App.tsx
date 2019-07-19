9f97633375644ec7f7487b6b868e95188fdf2413
Avi <aviklaiman@hotmail.com>
2019-07-19 18:22:53 +0300

    Real data

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -9,7 +9,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
-import { getNewsFlash, getNewsFlashes } from "../../api/mocks";
+import { getNewsFlash, getNewsFlashes } from "../../api";
 import NewsFlashDrawer from "../newsFlashDrawer";
 import { LatLng } from "leaflet";
 import * as ReactDOMServer from "react-dom/server";
