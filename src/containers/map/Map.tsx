b5f9bc358517126233c95c4d954aaf03b4e87aa4
Yossi <YossiDev25@Gmail.com>
2019-09-06 15:46:48 +0300

    first commit

--- src/containers/map/Map.tsx
+++ src/containers/map/Map.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import styles from "./Map.module.css";
-import { Map } from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import LeafletMarkerCluster from "../leafletClusterMarker";
-import ReactLeafletGoogleLayer from "react-leaflet-google-layer";
+import React from 'react';
+import styles from './Map.module.css';
+import { Map } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import LeafletMarkerCluster from '../leafletClusterMarker';
+import ReactLeafletGoogleLayer from 'react-leaflet-google-layer';
+import htmlToImage from 'html-to-image';
+import fileSaver from 'file-saver';
 /* This code is needed to properly load the images in the Leaflet CSS */
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 interface Props {
@@ -19,34 +21,57 @@ interface Props {
   leafletRef: React.Ref<any>;
 }
 
-const TempAnywayMap = (props: Props) => {
-  const position = [32.0461, 34.8516] as [number, number];
+const position = [32.0461, 34.8516] as [number, number];
 
-  return (
-    <div className={styles.mapContainer}>
-      <Map
-        style={{ height: "calc(100vh - 64px)", marginTop: "64px" }}
-        center={position}
-        zoom={12}
-        maxZoom={30}
-        preferCanvas={true}
-        zoomControl={false}
-        ref={props.leafletRef}
+class TempAnywayMap extends React.Component<Props> {
+  private mapRef: React.RefObject<HTMLInputElement>;
+
+  constructor(props: Props) {
+    super(props);
+    this.mapRef = React.createRef();
+  }
+
+  saveMapImage = (mapNode: any) => {
+    htmlToImage
+      .toPng(mapNode)
+      .then(dataURL => {
+        console.log(dataURL, 'fdsafsdfaasd');
+        fileSaver.saveAs(dataURL, 'mapImage.png');
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div
+        className={styles.mapContainer}
+        ref={this.mapRef}
+        onClick={this.saveMapImage.bind(this, this.mapRef.current)}
       >
-        <ReactLeafletGoogleLayer
-          googleMapsLoaderConf={{
-            KEY: "AIzaSyDUIWsBLkvIUwzLHMHos9qFebyJ63hEG2M",
-            VERSION: "3.37"
-          }}
-        />
-        <LeafletMarkerCluster
-          options={{ maxClusterRadius: 20, showCoverageOnHover: false }}
-          markers={props.newsFlashesMarkers}
-        />
-      </Map>
-    </div>
-  );
-};
+        <Map
+          style={{ height: 'calc(100vh - 64px)', marginTop: '64px' }}
+          center={position}
+          zoom={12}
+          maxZoom={30}
+          preferCanvas={true}
+          zoomControl={false}
+          ref={this.props.leafletRef}
+        >
+          <ReactLeafletGoogleLayer
+            googleMapsLoaderConf={{
+              KEY: 'AIzaSyDUIWsBLkvIUwzLHMHos9qFebyJ63hEG2M',
+              VERSION: '3.37'
+            }}
+          />
+          <LeafletMarkerCluster
+            options={{ maxClusterRadius: 20, showCoverageOnHover: false }}
+            markers={this.props.newsFlashesMarkers}
+          />
+        </Map>
+      </div>
+    );
+  }
+}
 
 const AnywayMap = React.forwardRef((props: any, ref) => (
   <TempAnywayMap leafletRef={ref} {...props} />
